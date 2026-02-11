27fbb32932c8fa452169bde7e404935314105a1f
Dhanvith Nayak <dhanvithnayak@proton.me>
2026-02-12 00:00:57 +0530

    make flask app compliant with vercel

--- backend/app.py
+++ backend/app.py
@@ -5,19 +5,14 @@ from config import CORS_ALLOWED_ORIGINS, DATA_FILE, FLASK_HOST, FLASK_PORT
 from api import videos_bp, analytics_bp
 
 
-def create_app():
-    app = Flask(__name__)
-    
-    CORS(app, origins=CORS_ALLOWED_ORIGINS)    
-    os.makedirs(os.path.dirname(DATA_FILE), exist_ok=True)
-    
-    app.register_blueprint(videos_bp)
-    app.register_blueprint(analytics_bp)
-    
-    return app
+app = Flask(__name__)
 
+CORS(app, origins=CORS_ALLOWED_ORIGINS)    
+os.makedirs(os.path.dirname(DATA_FILE), exist_ok=True)
+
+app.register_blueprint(videos_bp)
+app.register_blueprint(analytics_bp)
 
 if __name__ == "__main__":
-    app = create_app()
     # TODO: Run this in a WSGI server for prod
     app.run(host=FLASK_HOST, port=FLASK_PORT)
